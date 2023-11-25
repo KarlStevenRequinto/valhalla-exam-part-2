8f3b0e812011e2c3012d8cc46aa39ca8e6fe5aa5
KarlStevenRequinto <lerdrobott@gmail.com>
2023-11-25 18:40:02 +0800

    refractor ImageGridViewRenderer file

--- src/refactor-this/assets/js/ImageGridViewRenderer.js
+++ src/refactor-this/assets/js/ImageGridViewRenderer.js
@@ -1,111 +1,71 @@
 function ImageGridViewRenderer() { }
 
 ImageGridViewRenderer.prototype.render = function () {
-  function renderNatureImages(images) {
-    const natureView = document.getElementById("nature-images");
-    natureView.innerHTML += images.map(image => `
+  const mainView = document.getElementById("main-view");
+  const pageQueryParam = window.location.search.includes('page') ? Number(window.location.search.split('page=')[1]) : 1;
+
+  const renderImages = (images, idName) => {
+    const itemView = document.getElementById(`${idName}-images`);
+    if (!itemView) {
+      console.error(`Element with ID ${idName} not found.`);
+      return;
+    }
+
+    itemView.innerHTML += images.map(image => `
       <div class="col" style="height: 400px; padding: 10px;">
         <img class="image" src="${image.url}" alt="${image.name}" style="height: 100%; object-fit: cover; width: 100%;" />
         <div class="middle">
           <a class="btn btn-dark" href="${image.url}" download="${image.name}">DOWNLOAD</a>
         </div>
       </div>`).join('');
-  }
+  };
 
-  function renderArchitectureImages(images) {
-    const natureView = document.getElementById("architecture-images");
-    natureView.innerHTML += images.map(image => `
-      <div class="col" style="height: 400px; padding: 10px;">
-        <img class="image" src="${image.url}" alt="${image.name}" style="height: 100%; object-fit: cover; width: 100%;" />
-        <div class="middle">
-          <a class="btn btn-dark" href="${image.url}" download="${image.name}">DOWNLOAD</a>
-        </div>
-      </div>`).join('');
-  }
+  const renderImagesByCategory = async (category) => {
+    const validCategories = ['nature', 'architecture', 'fashion'];
 
-  function renderFashionImages(images) {
-    const natureView = document.getElementById("fashion-images");
-    natureView.innerHTML += images.map(image => `
-      <div class="col" style="height: 400px; padding: 10px;">
-        <img class="image" src="${image.url}" alt="${image.name}" style="height: 100%; object-fit: cover; width: 100%;" />
-        <div class="middle">
-          <a class="btn btn-dark" href="${image.url}" download="${image.name}">DOWNLOAD</a>
-        </div>
-      </div>`).join('');
-  }
+    if (!validCategories.includes(category)) {
+      console.error('Invalid category:', category);
+      return;
+    }
 
-  var nav =
-    '<nav class="navbar navbar-expand-lg navbar-light bg-light">'
-    + '  <a class="navbar-brand" href="#">Photo Sharing App</a>'
-    + '  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">'
-    + '    <span class="navbar-toggler-icon"></span>'
-    + '  </button>'
-    + '  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">'
-    + '    <div class="navbar-nav">'
-    + '      <a class="nav-link active" href="?nature">Nature <span class="sr-only">(current)</span></a>'
-    + '      <a class="nav-link" href="?architecture">Architecture</a>'
-    + '      <a class="nav-link" href="?fashion">Fashion</a>'
-    + '    </div>'
-    + '  </div>'
-    + '</nav>';
+    const categoryImages = document.createElement('div');
+    categoryImages.classList.add('container');
+    categoryImages.innerHTML = `<div id="${category}-images" class="row row-cols-3"></div>`;
+    mainView.appendChild(categoryImages);
 
-  document.getElementById("main-view").innerHTML = nav;
+    const getImagePromises = [
+      ImageDataGetter.getImages(category, (pageQueryParam * 3) - 2),
+      ImageDataGetter.getImages(category, (pageQueryParam * 3) - 1),
+      ImageDataGetter.getImages(category, pageQueryParam * 3)
+    ];
 
-  var page = 1;
-  if (window.location.search.includes('page')) {
-    page = Number(window.location.search.split('page=')[1]);
-  }
+    try {
+      const [images1, images2, images3] = await Promise.all(getImagePromises);
+      renderImages(images1, category);
+      renderImages(images2, category);
+      renderImages(images3, category);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   if (window.location.search.includes('?nature')) {
-    document.getElementById("main-view").innerHTML +=
-      '<div class="container">'
-      + '  <div id="nature-images" class="row row-cols-3"></div>'
-      + '</div>';
-    ImageDataGetter.getImages("nature", (page * 3) - 2)
-      .then(renderNatureImages);
-
-    ImageDataGetter.getImages("nature", (page * 3) - 1)
-      .then(renderNatureImages);
-
-    ImageDataGetter.getImages("nature", page * 3)
-      .then(renderNatureImages);
+    renderImagesByCategory("nature");
   } else if (window.location.search.includes('?architecture')) {
-    document.getElementById("main-view").innerHTML +=
-      '<div class="container">'
-      + '  <div id="architecture-images" class="row row-cols-3"></div>'
-      + '</div>';
-    ImageDataGetter.getImages("architecture", (page * 3) - 2)
-      .then(renderArchitectureImages);
-
-    ImageDataGetter.getImages("architecture", (page * 3) - 1)
-      .then(renderArchitectureImages);
-
-    ImageDataGetter.getImages("architecture", page * 3)
-      .then(renderArchitectureImages);
+    renderImagesByCategory("architecture");
   } else if (window.location.search.includes('?fashion')) {
-    document.getElementById("main-view").innerHTML +=
-      '<div class="container">'
-      + '  <div id="fashion-images" class="row row-cols-3"></div>'
-      + '</div>';
-    ImageDataGetter.getImages("fashion", (page * 3) - 2)
-      .then(renderFashionImages);
-
-    ImageDataGetter.getImages("fashion", (page * 3) - 1)
-      .then(renderFashionImages);
-
-    ImageDataGetter.getImages("fashion", page * 3)
-      .then(renderFashionImages);
+    renderImagesByCategory("fashion");
   }
 
-  var prevsearchstr = window.location.search.split('&page')[0] + '&page=' + (page - 1);
-  var nextsearchstr = window.location.search.split('&page')[0] + '&page=' + (page + 1);
-  var pagination =
-    '<nav>'
-    + '  <ul class="pagination">'
-    + '    <li class="page-item"><a class="page-link" href="' + prevsearchstr + '">Previous</a></li>'
-    + '    <li class="page-item"><a class="page-link" href="' + nextsearchstr + '">Next</a></li>'
-    + '  </ul>'
-    + '</nav>';
+  const prevsearchstr = window.location.search.split('&page')[0] + '&page=' + (pageQueryParam - 1);
+  const nextsearchstr = window.location.search.split('&page')[0] + '&page=' + (pageQueryParam + 1);
+  const pagination = `
+    <nav>
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="${prevsearchstr}">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="${nextsearchstr}">Next</a></li>
+      </ul>
+    </nav>`;
 
-  document.getElementById("main-view").innerHTML += pagination;
+  mainView.insertAdjacentHTML('beforeend', pagination);
 };
